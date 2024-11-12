dbc43d2f2c2a2f9cb7b2adcfd51d5ad1f212166d
toinoumlg <antoine.malanguerray@gmail.com>
2024-11-12 09:22:14 +0100

    colors

--- include/tester_ft_printf.h
+++ include/tester_ft_printf.h
@@ -1,8 +1,19 @@
 #ifndef TESTER_FT_PRINTF_H
 # define TESTER_FT_PRINTF_H
 
+
 #include "../../ft_printf.h"
 #include <fcntl.h>
+
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define BOLD "\033[1m"
+#define UNDERLINE "\033[4m"
 
 int main(int ac, char **av);
 void run_test_c();
